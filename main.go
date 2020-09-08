abf2e948f3ac203bb183e6cb62cd51160b936d66
Stéphane K <steffkarraz@hotmail.com>
2020-09-08 12:01:15 +0200

    Simple routine exec with sleep

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func inRoutine() {
+	fmt.Println("I'm printing from a routine")
+}
+
+func main() {
+	go inRoutine()
+	fmt.Println("I'm printing from the main process")
+	time.Sleep(1 * time.Second)
+	fmt.Println("I'm printing from the main process again after 1 second")
 }
