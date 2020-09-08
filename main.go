5c726ff6537dba2935fe8301c06ab2b26db070bf
Stéphane K <steffkarraz@hotmail.com>
2020-09-08 12:04:12 +0200

    Routine with channel waiting for message

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 
 func inRoutine(channel chan int) {
 	fmt.Println("I'm printing from a routine")
+	channel <- 0
 }
 
 func main() {
 	channel := make(chan int)
 
 	go inRoutine(channel)
+	sent := <-channel
 	fmt.Println("I'm printing from the main process with", sent)
 }
