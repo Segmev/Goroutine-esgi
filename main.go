efd18e4281a69f565eb2402b17e3b1a41641c3c4
Stéphane K <steffkarraz@hotmail.com>
2020-09-08 12:06:40 +0200

    Multiple routine with one channel

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 )
 
-func inRoutine(channel chan int) {
+func inRoutine(channel chan int, id int) {
 	fmt.Println("I'm printing from a routine")
-	channel <- 0
+	channel <- id
 }
 
 func main() {
 	channel := make(chan int)
 
-	go inRoutine(channel)
-	sent := <-channel
-	fmt.Println("I'm printing from the main process with", sent)
+	go inRoutine(channel, 0)
+	go inRoutine(channel, 1)
+	go inRoutine(channel, 2)
+	sent, sent2, sent3 := <-channel, <-channel, <-channel
+	fmt.Println("I'm printing from the main process with", sent, sent2, sent3)
 }
