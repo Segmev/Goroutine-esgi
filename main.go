91123b1851dec878b030367179fabb0827a64d29
Stéphane K <steffkarraz@hotmail.com>
2020-09-08 12:35:08 +0200

    I/O with a routine

--- main.go
+++ main.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func inRoutine(channel chan int, id int) {
-	fmt.Println("I'm printing from a routine")
-	channel <- id
+func waitingRoutine(channelIn chan string, channelOut chan string) {
+	fmt.Println("I'm the routine, I'll wait for a message")
+	msg := <-channelIn
+	fmt.Printf("Received this message:\n%s\n", msg)
+	fmt.Printf(".\n.\n.\n")
+	channelOut <- "Always been"
 }
 
 func main() {
-	channel := make(chan int)
+	channelIn, channelOut := make(chan string), make(chan string)
 
-	go inRoutine(channel, 0)
-	go inRoutine(channel, 1)
-	go inRoutine(channel, 2)
-	sent, sent2, sent3 := <-channel, <-channel, <-channel
-	fmt.Println("I'm printing from the main process with", sent, sent2, sent3)
+	go waitingRoutine(channelIn, channelOut)
+
+	fmt.Println("I'm the main process, I launched the routine and I'll wait some time")
+	time.Sleep(3 * time.Second)
+	fmt.Printf("I stopped waiting, sending a message...")
+	channelIn <- "Wait, it's that simple ?"
+	fmt.Println(<-channelOut)
 }
